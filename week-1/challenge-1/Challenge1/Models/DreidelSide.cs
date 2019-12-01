a1393610d5a2535aa17ba332c2d584b0f7486c82
Paul Shell <11514475+paul-shell@users.noreply.github.com>
2019-12-01 16:00:53 -0600

    Add solution for challenge 1.

---
+++ week-1/challenge-1/Challenge1/Models/DreidelSide.cs
@@ -0,0 +1,11 @@
+﻿namespace Challenge1.Models
+{
+    public class DreidelSide
+    {
+        public char Symbol { get; set; }
+
+        public string Name { get; set; }
+
+        public string Instructions { get; set; }
+    }
+}
